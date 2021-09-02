9eb53aedcfc6b3100b7fb4bcea6670a5afd2bb6f
Decagon <favour.chukwuemeka@decagon.dev>
2021-09-02 21:11:33 +0100

    adding utils abd certificates app

---
+++ apps/certificates/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from apps.utils.models import Timestamps
+
+
+class Certificates(Timestamps,  models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # created_at =
+    # updated_at
